c0f4c16be05c5bea93c60b392c37bf19ce97c0de
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2023-06-13 01:56:33 +0500

    first review fix tests

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards')
 
-router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
   res.send('Hi!')
-});
+}); */
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);

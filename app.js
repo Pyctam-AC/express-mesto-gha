cdb90a00e99cd9ff36f59cee980be53bc4f878f8
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2023-06-12 21:34:19 +0500

    first review fiw PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const mongoose = require('mongoose');
 
-const { PORT = 3002 } = process.env;
+const { PORT = 3000 } = process.env;
 //http://localhost:3002
 
 const app = express();
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '6485da795fb7954ee511993a' 
+    _id: '6485da795fb7954ee511993a'
   };
 
   next();

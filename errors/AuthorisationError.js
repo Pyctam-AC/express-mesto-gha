0820052c595a93c1d55b2987740e8f89b50ebcbb
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2023-06-23 20:49:44 +0500

    first review-fix test 23.06.2023

--- errors/AuthorisationError.js
+++ errors/AuthorisationError.js
@@ -1,8 +1,8 @@
-// const httpConstants = require('http2').constants;
+const httpConstants = require('http2').constants;
 // 401
 module.exports = class AuthorisationError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = 401;
+    this.statusCode = httpConstants.HTTP_STATUS_DENIED;
   }
 };
